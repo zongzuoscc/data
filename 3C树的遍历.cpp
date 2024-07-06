12863df2cc42da846a4a80891fc171c92d86f95c
YangYiming <2651575456@qq.com>
2024-07-06 22:00:14 +0800

    third

--- 3C树的遍历.cpp
+++ 3C树的遍历.cpp
@@ -14,88 +14,65 @@
 // 时，输出应为1 2 3
 // 输出为3 2 1时不会给判对！
 // 如果树唯一那么还是以先左子树再右子树的顺序
-
+#include <cstdio>
 #include <cstring>
 #include <iostream>
 using namespace std;
- 
+  
 const int maxn = 40;
-int pos1[maxn], pos2[maxn];// pos1 记录每个节点在前序遍历中的位置，pos2 记录每个节点在后序遍历中的位置
-int preorder[maxn], postorder[maxn];// preorder 存储前序遍历序列，postorder 存储后序遍历序列
-int L[maxn], size[maxn], R[maxn];// L 和 R 分别存储每个节点的左子节点和右子节点
-bool notonly = false;//记录是否唯一
-// 深度优先搜索，用于构建二叉树
+int pos1[maxn], pos2[maxn];
+int a1[maxn], a2[maxn];
+int L[maxn], size[maxn], R[maxn];
+bool book = false;
+  
 void dfs(int l, int r) {
     if (l >= r)
         return;
-    int root = preorder[l];// 当前子树的根节点
-    int lroot = preorder[l + 1];// 左子树的根节点（前序遍历的下一个节点）
-    int rroot = postorder[pos2[root] - 1];// 右子树的根节点（后序遍历中根节点的前一个节点）
-    if (lroot == rroot) //如果左子树根节点和右子树根节点相同
-    {
-        R[root] = rroot;// 将其作为右子树根节点
-        notonly = true;//说明不唯一
-        dfs(l + 1, r);//递归处理剩余节点
+    int root = a1[l];
+    int lroot = a1[l + 1];
+    int rroot = a2[pos2[root] - 1];
+    if (lroot == rroot) {
+        R[root] = rroot;
+        book = true;
+        dfs(l + 1, r);
         return;
     }
-    int lsize = pos1[rroot] - pos1[lroot];// 计算左子树的大小 就是右子树节点开始的位置减去左子树节点开始的位置
-    L[root] = lroot;// 记录左子树根节点
-    R[root] = rroot;// 记录右子树根节点
-    dfs(l + 1, l + lsize);//递归处理左子树
-    dfs(l + lsize + 1, r);//递归处理右子树
+    int lsize = pos1[rroot] - pos1[lroot];
+    L[root] = lroot;
+    R[root] = rroot;
+    dfs(l + 1, l + lsize);
+    dfs(l + lsize + 1, r);
 }
-
-//用于中序遍历
-void dfsmid(int now)//now表示当前节点
-{
+  
+void dfs3(int now) {
     if (L[now])
-        dfsmid(L[now]);//遍历左子树
-
-    cout << now << " ";//输出当前节点，以及空格
-
+        dfs3(L[now]);
+    cout << now << " ";
     if (R[now])
-        dfsmid(R[now]);//遍历右子树
-
-    if (now == preorder[1])//如果是根节点那么换行
+        dfs3(R[now]);
+    if (now == a1[1])
         cout << endl;
 }
-
-//关于先序，中序，后序遍历的代码部分，根据他们的名字就可以理解代码，
-//先序就先输出节点 然后递归左子树 最后递归右子树
-//中序就先递归左子树 然后输出节点 最后递归右子树
-//后序就先递归左子树 然后递归右子树 最后输出节点
- 
-int main()
-{
-    int n;//节点个数
+  
+int main() {
+    int n;
     cin >> n;
-    for (int i = 1; i <= n; i++) 
-    {
-        //size[i] = 1;
-        cin >> preorder[i];//输入先序的序列
-        pos1[preorder[i]] = i;//记录 先序节点的值 在后序序列中的位置
+    for (int i = 1; i <= n; i++) {
+        size[i] = 1;
+        cin >> a1[i];
+        pos1[a1[i]] = i;
     }
-    for (int i = 1; i <= n; i++) 
-    {
-        cin >> postorder[i];//输入后序的序列
-        pos2[postorder[i]] = i;//记录 后序节点的值 在后序序列中的位置
+    for (int i = 1; i <= n; i++) {
+        cin >> a2[i];
+        pos2[a2[i]] = i;
     }
- 
-    dfs(1, n);//把树构建好 并计算出树是否唯一
-    if (notonly)
+  
+    dfs(1, n);
+    if (book)
         cout << "No" << endl;
     else
         cout << "Yes" << endl;
-    dfsmid(preorder[1]);//根据构建好的树输出中序遍历的序列
-    for(int i=0;i<n;i++)
-    {
-        cout<<pos1[i]<<" ";
-    }
-    cout<<endl;
-        for(int i=0;i<n;i++)
-    {
-        cout<<pos2[i]<<" ";
-    }
-    cout<<endl;
+    dfs3(a1[1]);
+  
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,33 +1,64 @@
-// 哈夫曼树，第一行输入一个数n，表示叶结点的个数。需要用这些叶结点生成哈夫曼树，
-// 根据哈夫曼树的概念，这些结点有权值，即weight，题目需要输出所有叶子结点的路径长度与权值的乘积之和。
-// 输入有多组数据。
-// 每组第一行输入一个数n，接着输入n个叶节点（叶节点权值不超过100，2<=n<=1000）。
-// 输出权值。
+// 已知一颗树的节点间关系，请编程实现该树的先根遍历。
+//若干行，每行描述了一组双亲节点和孩子节点的关系序偶对（每个节点用不同的大写字母表示，节点小于26个）。且树的度小于5。
+// 该树的先根遍历序列，序列中每个字母用空格隔开。
+//输入
+// B E
+// B F
+// A B
+// A C
+//输出  A B E F C
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <cstring>
 using namespace std;
+const int MAX_NODE=26;
+vector<int> tree[MAX_NODE];//邻接表存树
+bool hasParent[MAX_NODE];//看每个节点是否有父节点
+
+void preorder(int node)
+{
+    cout<<(char)(node+'A')<<" ";
+    for(int i=0;i<tree[node].size();i++)
+    {
+        int child=tree[node][i];
+        preorder(child);
+    }
+}
+
+void postorder(int node)
+{
+    for(int i=0;i<tree[node].size();i++)
+    {
+        int child=tree[node][i];
+        postorder(child);
+    }
+    cout<<(char)(node+'A')<<" ";
+}
+
 int main()
 {
-    int n,sum,temp;
-    cin>>n;
-    vector<int> s;
-    for(int i=0;i<n;i++)
+    char parent,child;
+    memset(hasParent,false,sizeof hasParent);
+    while(cin>>parent>>child)
     {
-        cin>>temp;
-        s.push_back(temp);
+        int p=parent-'A';
+        int c=child-'A';
+        tree[p].push_back(c);
+        hasParent[c]=true;
     }
-    int st=0,ed=n;
-    while(ed-st!=1)
+    int root=-1;
+    for(int i=0;i<MAX_NODE;i++)
     {
-        sort(&s[st],&s[ed]);
-        temp=s[st++]+s[st++];
-        sum+=temp;
-        s.push_back(temp);
-        ed++;
+        if(!hasParent[i]&&!tree[i].empty())
+        {
+            root=i;
+            break;
+        }
     }
-    cout<<sum<<" ";
-    s.clear();
-    cout<<endl;
+    if(root!=-1)
+    {
+        preorder(root);
+        cout<<endl;
+    }
     return 0;
 }
